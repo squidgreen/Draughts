a3957310e308ef1fb67a40f271ca6b02eddc047b
Brandon <green.finite@gmail.com>
2016-07-31 23:10:32 -0700

    Basic Pair class and initial members of Piece class.

---
+++ Pair.java
@@ -0,0 +1,26 @@
+class Pair {
+  private int x;
+  private int y;
+
+  public Pair() {
+    setX(0);
+    setY(0);
+  }
+
+  /*
+   * name: Pair
+   * description: Constructs a Pair object with the given parameters
+   */
+  public Pair(int x, int y) {
+    setX(x);
+    setY(y);
+  }
+
+  private setX(int x) {
+    this.x = x;
+  }
+
+  private setY(int y) {
+    this.y = y;
+  }
+}
---
+++ Piece.java
@@ -0,0 +1,6 @@
+class Piece {
+  private Pair loc; // the piece's location
+  private int team; // the number of the player the piece belongs to
+
+
+}
