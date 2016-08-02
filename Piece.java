b9cdee5e86771e894502a838e27bef79ed78adc5
Brandon <green.finite@gmail.com>
2016-08-02 00:53:31 -0700

    Constructors, setters, and getters of the Piece class.

--- Piece.java
+++ Piece.java
@@ -1,6 +1,31 @@
+/*
+ * date: July 31, 2017
+ * author: Brandon Green
+ * description: A piece in a checker's game. Each piece belongs to a player and
+ *              occupies a square on the gameboard.
+ */
 class Piece {
   private Pair loc; // the piece's location
   private int team; // the number of the player the piece belongs to
 
+  public Piece() {
+    this.setLoc(new Pair());
+  }
 
+  public Piece(Pair startingLoc, int team) {
+    this.setLoc(new Pair(startingLoc));
+    this.setTeam(team);
+  }
+
+  public Piece(int x, int y, int team) {
+    Piece(new Pair(x, y), team); 
+  }
+
+  private void setLoc(Pair location) {
+    this.loc = location;
+  }
+
+  private void setTeam(int team) {
+    this.team = team;
+  }
 }
