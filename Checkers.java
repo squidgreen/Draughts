a88b029a2e30fb4b6008c4d56f5e9dcf24b46bc8
Brandon <green.finite@gmail.com>
2016-08-03 02:24:35 -0700

    Testing functionality

--- Checkers.java
+++ Checkers.java
@@ -2,7 +2,8 @@ public class Checkers {
 
   public static void main(String [] args) {
     GameBoard placeholder = new GameBoard();
-    displayBoard(gameBoard);
+    placeholder.setupBoard();
+    placeholder.displayBoard();
   } // end of main
 
   /*
@@ -26,11 +27,10 @@ public class Checkers {
     // prompt user
     System.out.println("Please enter the position of the piece to move:");
 
-    Scanner sc = new Scanner(System.in); // grab user input
-    int row = sc.nextInt();
-    int col = sc.nextInt();
+    //Scanner sc = new Scanner(System.in); // grab user input
+    //int row = sc.nextInt();
+    //int col = sc.nextInt();
 
-    if(gameBoard
   }
 
 }
--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,6 @@
 public class GameBoard {
   Piece[][] board;
+  int dimension = 2; // the width and height of the board
 
   /*
    * future --> Later this may take in a dimension for board size.
@@ -9,6 +10,15 @@ public class GameBoard {
       {new Piece(0,0,1), new Piece(1,1,1)},
       {new Piece(2,2,2), new Piece(3,3,2)}
     };
+  }
+
+  public void displayBoard() {
+    for(int row = 0; row < dimension; row++) { // we pick a row...
+      for(int col = 0; col < dimension; col++) { // then go through each column
+        System.out.print(board[row][col].getTeam() + " "); // print team number
+      }
+      System.out.println();
+    }
   }
 
   /* 
