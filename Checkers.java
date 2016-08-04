5236024b935a049f2d9ed931f2b5eaa19dadf7da
Brandon <green.finite@gmail.com>
2016-08-03 23:48:54 -0700

    Removed redundant methods that were placed into more appropriate classes.

--- Checkers.java
+++ Checkers.java
@@ -6,23 +6,6 @@ public class Checkers {
     placeholder.displayBoard();
   } // end of main
 
-  /*
-   * Name: displayBoard
-   * Description: Prints out the current board row by row
-   */
-  public static void displayBoard(int[][] gameBoard) {
-    for(int i = 0; i < 8; i++) {
-      for(int j = 0; j < 8; j++) {
-        if(j < 7) {
-          System.out.print(gameBoard[i][j] + ", ");
-        } else {
-          System.out.print(gameBoard[i][j]);
-        }
-      }
-      System.out.println(); // newline after each array is printed
-    }
-  }
-
   public static void selectPiece(int player) {
     // prompt user
     System.out.println("Please enter the position of the piece to move:");
