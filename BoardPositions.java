38d094857664ba67d2a0764c7f00c7cc1b39c523
Brandon <green.finite@gmail.com>
2016-09-07 22:54:56 -0700

    BoardPositions class that combines two ArrayLists for tracking positions a Piece can move.

--- BoardPositions.java
+++ BoardPositions.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * This class stores at what positions a piece on the GameBoard can move to. It
  * differentiates between positions that a piece can simply move to and
  * positions that would score a point for that player.
+ *
  */
-class BoardPositions {
+public class BoardPositions {
   private ArrayList<Pair> validLocs; // positions the piece can move to
   private ArrayList<Pair> scoringLocs; // positions the piece can move to and score
 
@@ -25,12 +27,45 @@ class BoardPositions {
    * @param type      signifies which ArrayList to add the Pair to
    * @return          none
    */
-  private void add(Pair somePair, String type) { // TODO you need these
+  public void add(Pair somePair, String type) { // TODO you need these
     if(type.equals("valid")) {
       validLocs.add(somePair);
     } else if(type.equals("scoring")) {
       scoringLocs.add(somePair);
     }
+  }
+
+  public boolean contains(Object o) {
+    if(validLocs.contains(o) || scoringLocs.contains(o)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /*
+   * Print out the contents of the valid locations ArrayList
+   */
+  public void printValidPositions() {
+    ListIterator iter = validLocs.listIterator();
+
+    while(iter.hasNext()) {
+      ((Pair)iter.next()).print(); // utilizing Pair's print method
+      System.out.println();
+    }
+  }
+
+  /*
+   * Print out the contents of the scoring locations ArrayList
+   */
+  public void printScoringPositions() {
+    ListIterator iter = scoringLocs.listIterator();
+
+    while(iter.hasNext()) {
+      ((Pair)iter.next()).print();
+      System.out.println();
+    }
+
   }
 
 }
